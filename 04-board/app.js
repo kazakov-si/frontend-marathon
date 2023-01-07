26bceae5c65760ca4835667dd368b9c12369939e
Sergey Kazakov <62552054+kazakov-si@users.noreply.github.com>
2023-01-07 12:20:44 +0300

    Add files via upload

---
+++ 04-board/app.js
@@ -0,0 +1,28 @@
+const board = document.querySelector('#board')
+const colors = ['#2e1114', '#501b1d', '#64485c', '#83677b', '#adadad', '#6f2232', '#950740', '#c3073f']
+const SQUARES_NUMBER = 600
+
+for (let i = 0; i < SQUARES_NUMBER; i++) {
+    const square = document.createElement('div')
+    square.classList.add('square')
+
+    square.addEventListener('mouseover', () => setColor(square))
+    square.addEventListener('mouseleave', () => removeColor(square))
+    board.append(square)
+}
+
+function setColor(element) {
+    const color = getRandomColor()
+    element.style.backgroundColor = color
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+}
+
+function removeColor(element) {
+    element.style.backgroundColor = '#1d1d1d'
+    element.style.boxShadow = `0 0 2px #000`
+}
+
+function getRandomColor() {
+    const index = Math.floor(Math.random() * colors.length)
+    return colors[index]
+}
